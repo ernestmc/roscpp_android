489f0fe9568da5366e3a410394f1a02dbc33b27d
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2016-06-16 11:22:25 -0300

    Updated gcc version.

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 system=$(uname -s | tr 'DL' 'dl')-$(uname -m)
-gcc_version=4.6
+gcc_version=4.8
 toolchain=arm-linux-androideabi-$gcc_version
 platform=android-14
 PYTHONPATH=/opt/ros/indigo/lib/python2.7/dist-packages:$PYTHONPATH
@@ -8,4 +8,3 @@ CMAKE_BUILD_TYPE=Debug
 # Enable this if you need to use pluginlib in Android.
 # The plugins will be statically linked
 use_pluginlib=1
-
